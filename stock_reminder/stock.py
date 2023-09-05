1f13bfbe786d2b3db175dc345783e7b2226eee0f
Developer Localhost <developer@local.host>
2023-09-05 08:35:21 -0500

    Testing for exceptions..

--- stock_reminder/stock.py
+++ stock_reminder/stock.py
@@ -4,4 +4,7 @@ class Stock:
         self.price = None
     
     def update(self, timestamp, price):
+        if price < 0:
+            raise ValueError("price should not be negative")
         self.price = price
+
