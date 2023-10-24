70f59dd43d78237504b7751168daee08fdd9e5c4
Developer Localhost <developer@local.host>
2023-10-24 11:23:03 -0500

    Refactoring the design

--- stock_reminder/stock.py
+++ stock_reminder/stock.py
@@ -1,10 +1,14 @@
 class Stock:
+    @property
+    def price(self):
+        return self.price_history[-1] if self.price_history else None
+
     def __init__(self, symbol):
         self.symbol = symbol
-        self.price = None
+        self.price_history = []
     
     def update(self, timestamp, price):
         if price < 0:
             raise ValueError("price should not be negative")
-        self.price = price
+        self.price_history.append(price)
 
