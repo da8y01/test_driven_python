2bb678cf9e1e605a25b392a7eec4187aea656fd2
Developer Localhost <developer@local.host>
2024-01-30 10:22:54 -0500

    Replace Magic Literals with Constants and Commenting Styles

--- stock_reminder/stock.py
+++ stock_reminder/stock.py
@@ -13,6 +13,9 @@ class StockSignal(Enum):
 
 
 class Stock:
+    LONG_TERM_TIMESPAN = 10
+    SHORT_TERM_TIMESPAN = 5
+
     def __init__(self, symbol):
         self.symbol = symbol
         self.price_history = []
@@ -31,7 +34,8 @@ class Stock:
     
     def get_crossover_signal(self, on_date):
         closing_price_list = []
-        for i in range(11):
+        NUM_DAYS = self.LONG_TERM_TIMESPAN + 1
+        for i in range(NUM_DAYS):
             chk = on_date.date() - timedelta(i)
             for price_event in reversed(self.price_history):
                 if price_event.timestamp.date() > chk:
@@ -43,11 +47,9 @@ class Stock:
                     closing_price_list.insert(0, price_event)
                     break
 
-        # Return NEUTRAL signal
         if len(closing_price_list) < 11:
-            return 0
+            return StockSignal.neutral
 
-        # BUY signal
         if (sum([update.price
                     for update in closing_price_list[-11:-1]])/10
                 > sum([update.price
@@ -56,9 +58,8 @@ class Stock:
                         for update in closing_price_list[-10:]])/10
                 < sum([update.price
                         for update in closing_price_list[-5:]])/5):
-                    return 1
+                    return StockSignal.buy
 
-        # BUY signal
         if (sum([update.price
                     for update in closing_price_list[-11:-1]])/10
                 < sum([update.price
@@ -67,7 +68,6 @@ class Stock:
                         for update in closing_price_list[-10:]])/10
                 > sum([update.price
                         for update in closing_price_list[-5:]])/5):
-                    return -1
+                    return StockSignal.sell
 
-        # NEUTRAL signal
-        return 0
+        return StockSignal.neutral
