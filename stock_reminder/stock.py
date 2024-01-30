93c1782ee49f31722b795dc21f1fe1daa1bb8c06
Developer Localhost <developer@local.host>
2024-01-30 10:31:44 -0500

    The Extract Method refactoring

--- stock_reminder/stock.py
+++ stock_reminder/stock.py
@@ -32,10 +32,9 @@ class Stock:
     def is_increasing_trend(self):
         return self.price_history[-3].price < self.price_history[-2].price < self.price_history[-1].price
     
-    def get_crossover_signal(self, on_date):
+    def _get_closing_price_list(self, on_date, num_days):
         closing_price_list = []
-        NUM_DAYS = self.LONG_TERM_TIMESPAN + 1
-        for i in range(NUM_DAYS):
+        for i in range(num_days):
             chk = on_date.date() - timedelta(i)
             for price_event in reversed(self.price_history):
                 if price_event.timestamp.date() > chk:
@@ -46,6 +45,12 @@ class Stock:
                 if price_event.timestamp.date() < chk:
                     closing_price_list.insert(0, price_event)
                     break
+        return closing_price_list
+
+    def get_crossover_signal(self, on_date):
+        NUM_DAYS = self.LONG_TERM_TIMESPAN + 1
+        closing_price_list = \
+            self._get_closing_price_list(on_date, NUM_DAYS)
 
         if len(closing_price_list) < 11:
             return StockSignal.neutral
