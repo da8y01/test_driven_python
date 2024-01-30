fc7aa7547991893681c1c232ef00c61a06e68ef3
Developer Localhost <developer@local.host>
2024-01-30 13:36:38 -0500

    Chap 3 Exercise

--- stock_reminder/timeseries.py
+++ stock_reminder/timeseries.py
@@ -2,15 +2,19 @@ import bisect
 import collections
 from datetime import timedelta
 
-
 Update = collections.namedtuple("Update", ["timestamp", "value"])
 
+
+class NotEnoughDataException(Exception):
+    pass
+
+
 class TimeSeries:
-    def __getitem__(self, index):
-        return self.series[index]
-    
     def __init__(self):
         self.series = []
+
+    def __getitem__(self, index):
+        return self.series[index]
 
     def update(self, timestamp, value):
         bisect.insort_left(self.series, Update(timestamp, value))
@@ -29,3 +33,16 @@ class TimeSeries:
                     closing_price_list.insert(0, price_event)
                     break
         return closing_price_list
+
+
+class MovingAverage:
+    def __init__(self, series, timespan):
+        self.series = series
+        self.timespan = timespan
+
+    def value_on(self, end_date):
+        moving_avg_series = self.series.get_closing_price_list(end_date, self.timespan)
+        if len(moving_avg_series) < self.timespan:
+            raise NotEnoughDataException("Not enough data to calculate moving average")
+        price_list = [update.value for update in moving_avg_series]
+        return sum(price_list)/self.timespan
