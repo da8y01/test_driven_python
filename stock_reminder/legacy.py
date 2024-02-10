31f6402339a88c31a3e23aa46de1cd63e2b84e67
Developer Localhost <developer@local.host>
2024-02-09 21:25:26 -0500

    Chapter 5. Extract the method and stub

--- stock_reminder/legacy.py
+++ stock_reminder/legacy.py
@@ -12,23 +12,29 @@ class AlertProcessor:
         rule_1 = PriceRule("GOOG", lambda stock: stock.price > 10)
         rule_2 = PriceRule("AAPL", lambda stock: stock.price > 5)
         self.exchange["GOOG"].updated.connect(
-            lambda stock: print(stock.symbol, stock.price) \
-                if rule_1.matches(self.exchange) else None)
+            lambda stock: self.print_action(stock, rule_1))
         self.exchange["AAPL"].updated.connect(
-            lambda stock: print(stock.symbol, stock.price) \
-                if rule_2.matches(self.exchange) else None)
+            lambda stock: self.print_action(stock, rule_2))
         if autorun:
             self.run()
 
     def run(self):
-        updates = []
-        with open("updates.csv", "r") as fp:
-            for line in fp.readlines():
-                symbol, timestamp, price = line.split(",")
-                updates.append((symbol, datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f"), int(price)))
+        updates = self.parse_file()
         self.do_updates(updates)
 
     def do_updates(self, updates):
         for symbol, timestamp, price in updates:
             stock = self.exchange[symbol]
             stock.update(timestamp, price)
+
+    def parse_file(self):
+        updates = []
+        with open("updates.csv", "r") as fp:
+            for line in fp.readlines():
+                symbol, timestamp, price = line.split(",")
+                updates.append((symbol, datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f"), int(price)))
+        return updates
+    
+    def print_action(self, stock, rule):
+        print(stock.symbol, stock.price) \
+            if rule.matches(self.exchange) else None
